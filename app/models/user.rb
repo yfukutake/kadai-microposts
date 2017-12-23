8df8678432105e3c39469675b70af3781593a340
EC2 Default User <ec2-user@ip-172-31-27-127.us-east-2.compute.internal>
2017-12-23 17:05:02 +0000

    user login

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+	before_save {self.email.downcase!}
+	validates :name, presence: true, length: {maximum: 50 }
+	validates :email, presence: true, length: {maximum: 255 }
+	has_secure_password
+end
